7d7ad86116616dbe7558cea29d916b639cee50f4
Oleg Katyshkin <olegkatyskin@gmail.com>
2024-09-17 12:59:56 +0300

    added vectorized swap

--- normalize.py
+++ normalize.py
@@ -3,7 +3,7 @@ banned_symbols = ['"', "«", "»", "\t", " ", "'", "`"]
 
 
 with open("onegin_files/onegin") as f:
-    with open("onegin_files/onegin_normalzed_2", "w") as out:
+    with open("onegin_files/onegin_normalized.txt", "w") as out:
         for line in f:
             if all(x not in line for x in banned_letters):
                 for symbol in banned_symbols:
